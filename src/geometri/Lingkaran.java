2e4e8d9bb3589914cdfb03ae5f7b43206ab766e6
aryyudha.project <aryyudhanugroho03@gmail.com>
2023-06-13 19:44:04 +0700

    Membuat class Lingkaran dan PersegiPanjang

---
+++ src/geometri/Lingkaran.java
@@ -0,0 +1,16 @@
+
+package geometri;
+
+
+public class Lingkaran {
+        
+        double luas,keliling;
+        
+    public Lingkaran(double radius) {
+    this.luas = luas;
+    this.keliling = keliling;
+}
+  public double radius(){
+    return radius;
+}
+}
